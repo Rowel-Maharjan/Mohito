9657601c838fe15a01888570123b474988b9a959
Rowel Maharjan <rowelmhj@gmail.com>
2025-06-21 12:32:10 +0545

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return <div className="flex-center h-[100vh]">App</div>;
+  return (
+    <main>
+      <Navbar />
+      <div className="h-[1000px]"></div>
+    </main>
+  );
 };
 
 export default App;
