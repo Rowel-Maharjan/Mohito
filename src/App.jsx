b798c28b4e52d56c0170345a860ff5dad92bab4e
Rowel Maharjan <rowelmhj@gmail.com>
2025-06-26 18:50:07 +0545

    Menu section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import ArtSection from "./components/ArtSection";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -17,6 +18,7 @@ const App = () => {
       <Cocktails />
       <About />
       <ArtSection />
+      <Menu />
     </main>
   );
 };
